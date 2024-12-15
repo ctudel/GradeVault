91ed6ca2928e1ff24b0f27da2606b72cf3fef221
ctudel <christudela@u.boisestate.edu>
2024-12-14 17:10:16 -0700

    Created driver skeleton

---
+++ Driver.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Driver {
+
+  public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+
+    while (true) {
+      System.out.println("Welcome to the grade book");
+      System.out.println("-------------------------");
+      System.out.println("Select a class...\n");
+      scnr.nextLine();
+      System.out.println();
+    }
+  }
+
+}
